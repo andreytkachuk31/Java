a1c2d9ca1f50c09b7ff6a26dd437984e3bbba6ca
Andrii Tkachuk <andrey.tkachuk31@gmail.com>
2015-11-06 15:08:38 +0200

    Fix pom structure

---
+++ EPAM_CDP/Task4/src/test/java/com/epam/core/storage/FileStorageIntegrationTest.java
@@ -0,0 +1,16 @@
+package com.epam.core.storage;
+
+import org.junit.Test;
+
+/**
+ * @author Andrii_Tkachuk
+ * @since 11/6/2015
+ */
+public class FileStorageIntegrationTest {
+
+    @Test
+    public void a() {
+
+    }
+
+}
