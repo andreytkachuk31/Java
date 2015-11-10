1043f203c02829d5033b46c7e9b4589935f9e762
Andrii Tkachuk <andrey.tkachuk31@gmail.com>
2015-11-10 17:57:33 +0200

    Add integration test

--- EPAM_CDP/Task4/src/test/java/com/epam/core/integration/storage/FileStorageIntegrationTest.java
+++ EPAM_CDP/Task4/src/test/java/com/epam/core/integration/storage/FileStorageIntegrationTest.java
@@ -1,6 +1,15 @@
 package com.epam.core.integration.storage;
 
-import org.junit.Test;
+import com.epam.bootstrap.builder.FileStorageBuilder;
+import com.epam.bootstrap.builder.StorageBuilder;
+import com.epam.core.unit.storage.Storage;
+import org.junit.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.junit.Assert.*;
 
 /**
  * @author Andrii_Tkachuk
@@ -8,9 +17,51 @@ import org.junit.Test;
  */
 public class FileStorageIntegrationTest {
 
-    @Test
-    public void a() {
+    private static final String TEST_FILE_PREFIX = "test_storage";
+    private static final String TEST_FILE_SUFFIX = ".txt";
 
+    private static final String KEY = "key";
+    private static final String VALUE = "value";
+    private static final String UPDATE_VALUE = "updateValue";
+
+    private Path path;
+
+    private Storage storage;
+
+    @Before
+    public void before() throws IOException {
+        path = Files.createTempFile(TEST_FILE_PREFIX, TEST_FILE_SUFFIX);
+        StorageBuilder storageBuilder = new FileStorageBuilder(path);
+        storage = storageBuilder.build();
     }
 
+    @After
+    public void after() throws IOException {
+        Files.deleteIfExists(path);
+    }
+
+    @Test
+    public void shouldCreate() {
+        storage.create(KEY, VALUE);
+
+        storage.read(KEY);
+    }
+
+    @Test
+    public void shouldUpdate() {
+        storage.create(KEY, VALUE);
+
+        storage.update(KEY, UPDATE_VALUE);
+
+        String value = storage.read(KEY);
+
+        assertEquals(value, UPDATE_VALUE);
+    }
+
+    @Test
+    public void shouldRemove() {
+        storage.create(KEY, VALUE);
+
+        storage.remove(KEY);
+    }
 }
