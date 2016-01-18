e2fa5ff8348cee3b043effa9d3240db666657af5
andrey.tkachuk <andrey.tkachuk31@gmail.com>
2016-01-18 12:43:54 -0800

    Refactoring Rest Module

--- Spring/Spring REST/src/main/java/controller/PersonRestController.java
+++ Spring/Spring REST/src/main/java/controller/PersonRestController.java
@@ -4,9 +4,11 @@ import model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import service.PersonService;
 
 @Controller
@@ -17,18 +19,12 @@ public class PersonRestController {
     private PersonService personService;
 
     @RequestMapping("person/{id}")
-    public Person getById(@PathVariable Long id) {
+    public @ResponseBody Person getById(@PathVariable Long id) {
         return personService.getById(id);
     }
-
-    @RequestMapping(value="person", params="id")
-    public Person getByIdFromParam(@RequestParam("id") Long id) {
-        return personService.getById(id);
-    }
-
 
     @RequestMapping(value="person", method= RequestMethod.POST)
-    public String savePerson(Person person) {
+    public @ResponseBody String savePerson(@RequestBody Person person) {
         personService.save(person);
         return "Saved person: " + person.toString();
     }
